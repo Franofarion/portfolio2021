d94286f3bdd9565ac9413ff77835f4a9bb474f00
Domange <baptiste.domange@gmail.com>
2021-07-30 19:32:12 +0200

    rocket component

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-import Main from './components/Main';
+import "./App.css";
+import Main from "./components/Main";
+import Rocket from "./components/Rocket/Rocket";
 
 function App() {
   return (
     <div className="App">
+      <Rocket />
       <Main />
     </div>
   );
---
+++ front/src/components/Rocket/Rocket.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactComponent as ReactRocket } from "./rocket.svg";
+import "./Rocket.css";
+
+export default function Rocket() {
+  return (
+    <>
+      <ReactRocket className="Rocket" />
+    </>
+  );
+}
